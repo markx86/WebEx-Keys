005694cba5bb6149d3952b07204dbc1d60920357
MadMark <markedenergy@gmail.com>
2021-04-12 17:59:43 +0200

    Fixed annoying bug
    Fixed bug where the playhead controls wouldn't work when in fullscreen

--- src/webex-keys/main.js
+++ src/webex-keys/main.js
@@ -5,6 +5,7 @@ const SETTINGS_BTN = "playerSetting";
 const FULLSCREEN_BTN = "fullScreen";
 const TIME_INDICATOR = "timeIndicator";
 const BASE_BAR = "baseBar";
+const SCREEN = "screen_html5_api"
 
 // Classes
 const SPEEDS_CONTAINER_CLASS = "setting-item-speed";
@@ -76,21 +77,25 @@ function clamp(value, min, max)
 function movePlayhead(direction) 
 {
     try {
-        var rawTime = document.getElementById(TIME_INDICATOR).innerHTML.split(TIME_SPLIT[0])[1].split(TIME_SPLIT[1]);
-        var videoLength = 0;
-        var i;
-        for (i = 0; i < rawTime.length; i ++)
-            videoLength += parseInt(rawTime[rawTime.length - i - 1]) * Math.pow(60, i);
-        var movement = (document.getElementById(BASE_BAR).getBoundingClientRect().width / videoLength) * SECONDS * direction;
-        var box = document.getElementsByClassName(PLAYHEAD_CLASS)[0].getBoundingClientRect();
-        var coords = [box.x + (box.width / 2), box.y + (box.height / 2)];
-        document.getElementsByClassName(SLIDER_CLASS)[0].dispatchEvent(new MouseEvent("mousedown", {bubbles: true, cancelable: true, view: window, clientX: coords[0], clientY: coords[1]}));
-        setTimeout(function() {
-            document.getElementsByClassName(SLIDER_CLASS)[0].dispatchEvent(new MouseEvent("mousemove", {bubbles: true, cancelable: true, view: window, clientX: coords[0] + movement, clientY: coords[1]}));
-            setTimeout(function() {
-                document.getElementsByClassName(SLIDER_CLASS)[0].dispatchEvent(new MouseEvent("mouseup", {bubbles: true, cancelable: true, view: window, clientX: coords[0] + movement, clientY: coords[1]}));
-            }, TIMEOUT);
-        }, TIMEOUT);
+		document.getElementById(SCREEN).dispatchEvent(new MouseEvent("mousemove"));
+		setTimeout(function() {
+			var rawTime = document.getElementById(TIME_INDICATOR).innerHTML.split(TIME_SPLIT[0])[1].split(TIME_SPLIT[1]);
+			var videoLength = 0;
+			var i;
+			for (i = 0; i < rawTime.length; i ++)
+				videoLength += parseInt(rawTime[rawTime.length - i - 1]) * Math.pow(60, i);
+			var movement = (document.getElementById(BASE_BAR).getBoundingClientRect().width / videoLength) * SECONDS * direction;
+			var box = document.getElementsByClassName(PLAYHEAD_CLASS)[0].getBoundingClientRect();
+			var coords = [box.x + (box.width / 2), box.y + (box.height / 2)];
+			var slider = document.getElementsByClassName(SLIDER_CLASS)[0];
+			slider.dispatchEvent(new MouseEvent("mousedown", {bubbles: true, cancelable: true, view: window, clientX: coords[0], clientY: coords[1]}));
+			setTimeout(function() {
+				slider.dispatchEvent(new MouseEvent("mousemove", {bubbles: true, cancelable: true, view: window, clientX: coords[0] + movement, clientY: coords[1]}));
+				setTimeout(function() {
+					slider.dispatchEvent(new MouseEvent("mouseup", {bubbles: true, cancelable: true, view: window, clientX: coords[0] + movement, clientY: coords[1]}));
+				}, TIMEOUT);
+			}, TIMEOUT);
+		}, TIMEOUT);
 
     } catch (err) {
         console.error("[WebEx Automator -> movePlayhead] " + err.message)
